84a0d41e5fb55a5d3cede97d0113febbf27b6fd9
Vineet <naikvin@gmail.com>
2013-06-10 23:32:21 +0530

    Add cli.py with utilities for writing command line scripts

---
+++ cli.py
@@ -0,0 +1,40 @@
+"""Some random utils for writing command line scripts"""
+
+import os
+import sys
+from contextlib import contextmanager
+
+
+class CliError(Exception):
+    """A base cli exception"""
+    pass
+
+
+@contextmanager
+def read_input(filepath, stdin):
+    """Contextmanager that gets a file like object as source
+
+    If filepath is not None, then it will open the file and return the
+    file object. In the end, takes care of closing it. Reads standard
+    input if stdin is True Raises an exception if both above cases
+    fail
+
+    :param filepath: str
+
+    :param stdin: bool
+
+    :rtype: file like object to be used with `with` keyword
+
+    """
+    if filepath is not None:
+        f = open(os.path.abspath(filepath))
+        yield f
+        f.close()
+    elif stdin:
+        yield sys.stdin.readlines()
+    else:
+        raise CliError(
+            'At least one of log file path or'
+            'input from stdin is required'
+        )
+
