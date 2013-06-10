12b771d05428fd05fdbd49d22ddfa74c74f66eb0
Vineet <naikvin@gmail.com>
2013-06-10 23:32:36 +0530

    Refactor and improve splitlogs using argparse

--- splitlogs.py
+++ splitlogs.py
@@ -1,93 +1,88 @@
-#!/usr/bin/env python
+"""A command line script to split large log files by date
 
-import sys
-import os
-import re
-
-
-def usage():
-    return """
-======================================================================
-Command line tool to split a huge apache2 log file into smaller chunks
-by date
-======================================================================
-
-Author: Vineet Naik <naikvin@gmail.com> @naiquevin
-
-Usage
-=====
-
-For splitting log file
-
-    $ python splitlogs.py <filepath> <date_criteria>
-
-      filepath: absolute path to the log file
-      date_criteria: of format */*/* (eg. 06/Nov/*, */Nov/*)      
-
-For running tests
-
-    $ python splitlogs.py test
-
-For help
-
-    $ python splitlogs.py help
+   Author: Vineet Naik <naikvin@gmail.com>
 
 """
 
+import re
+import argparse
+import cli
 
-def parse_criteria(criteria):
-    day, month, year = criteria.split('/')
+
+LOG_PATTERN_FORMATS = {
+    'apache2_access': r'^.*\[{day}\/{month}\/{year}.+',
+    'apache2_error': r'^\[.+\s{month}\s{day}\s.+\s{year}\]\s.+'
+}
+
+
+def parse_date(date):
+    """Extract out day/month/year from the date wildcard pattern as a dict
+
+    :param date: str of type */*/* eg. */Jun/*
+
+    :rtype: dict
+
+    """
+    day, month, year = date.split('/')
     return dict((('day', '\d{2}' if day == '*' else day),
                 ('month', '[A-Za-z]' if month == '*' else month.title()),
                 ('year', '\d{4}' if year == '*' else year)))
 
 
-def get_criteria_pattern(crt, logtype='access'):
-    if logtype == 'error':
-        pattern = re.compile(r'^\[.+\s%s\s%s\s.+\s%s\]\s.+' % (crt['month'], crt['day'], crt['year']))
-    else:
-        pattern = re.compile(r'^.*\[%s\/%s\/%s.+$' % (crt['day'], crt['month'], crt['year']))
-    return pattern
+def get_date_pattern(date_pattern, log_type='apache2_access'):
+    """Get the correct regex pattern to match date from the logs
+
+    :param date_pattern: dict
+
+    :param log_type: type of logs
+
+    :rtype: SRE_Pattern object
+
+    """
+    return re.compile(LOG_PATTERN_FORMATS[log_type].format(**date_pattern))
 
 
 def test():
-    c = parse_criteria('*/nov/*')
+    """Run tests"""
+    c = parse_date('*/nov/*')
     assert c['day'] == '\d{2}'
     assert c['month'] == 'Nov'
     assert c['year'] == '\d{4}'
 
-    cp = get_criteria_pattern(c)
+    cp = get_date_pattern(c)
     log = '183.82.25.178 - - [08/Nov/2012:13:15:05 +0000] "GET /favicon.ico HTTP/1.1" 404 503 "-" "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:16.0) Gecko/20100101 Firefox/16.0"'
     assert cp.match(log) is not None
 
     print 'tests: ok'
 
 
-def handle_subcommand(argv):
+def split(args):
+    pattern = get_date_pattern(parse_date(args.date), args.log_type)
+
     try:
-        subcommand = argv[1]
-    except IndexError:
-        subcommand = 'help'
-    if subcommand == 'test':
-        test()
-    else:
-        print usage()
-    exit(0)
+        with cli.read_input(args.filepath, args.stdin) as f:
+            print ''.join(line for
+                          line in f
+                          if pattern.match(line) is not None)
+    except cli.CliError as e:
+        raise argparse.ArgumentError(args.filepath, str(e))
 
 
 if __name__ == '__main__':
-    try:
-        logfile, criteria = sys.argv[1:3]
-        try:
-            logtype = sys.argv[3]
-        except IndexError:
-            logtype = 'access'
-    except ValueError:
-        handle_subcommand(sys.argv)
+    # test()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('date', help='Wild card pattern for date eg. 06/Nov/*, */Nov/*')
+    parser.add_argument('-f', '--filepath',
+                        help='path to the log file')
+    parser.add_argument('-i', '--stdin',
+                        help='Use standard input', action='store_true')
+    parser.add_argument('-t', '--log-type',
+                        help=(
+                            'Regex pattern or name of a '
+                            'predefined log pattern format for parsing logs'
+                        ), default='apache2_access',
+                        choices=LOG_PATTERN_FORMATS.keys())
 
-    pattern = get_criteria_pattern(parse_criteria(criteria), logtype)
-    with open(os.path.abspath(logfile)) as f:
-        lines = f
-        lines = (line for line in lines if pattern.match(line) is not None)
-        print ''.join(list(lines))
+    args = parser.parse_args()
+    split(args)
 
