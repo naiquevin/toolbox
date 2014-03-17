6ce702a4f4ec649aceb1c34c2913c4240e1bd210
Vineet <naikvin@gmail.com>
2014-03-18 00:14:55 +0530

    Remove dependency on docopt and cli.py in histit

--- histit.py
+++ histit.py
@@ -1,25 +1,31 @@
 """A script to quickly create histograms from data in text files
 
-Usage: histit.py ( show | save ) ( -i | FILE ) -t TITLE -x XLABEL [ -d DTYPE -b BINS ]
-       histit.py -h | --help | --version
+usage: histit.py [-h] [-d DATAFILE] [-a {show,save}] [-t {int,float}]
+                 [-b BINS]
+                 title xlabel
 
-Options:
-    -t --title TITLE      Title of the histogram
-    -x --xlabel XLABEL    The x-axis label
-    -d --datatype DTYPE   Datatype [default: int]
-    -b --bins BINS        No. of bins [default: 10]
-    -h --help             Show help
-    --version             Show version
+positional arguments:
+  title                 The title of the histogram
+  xlabel                The label on the X-axis
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DATAFILE, --datafile DATAFILE
+                        Path to the input datafile
+  -a {show,save}, --action {show,save}
+                        The action to carry out
+  -t {int,float}, --type {int,float}
+                        Type of input on each line expected
+  -b BINS, --bins BINS  No. of bins
 
 """
 
 import os
 import sys
+import select
+import argparse
 
-from docopt import docopt
 import matplotlib.pyplot as plt
-
-from cli import read_input
 
 
 def plot_hist(data, title, xlabel, bins):
@@ -35,19 +41,41 @@ def cast_to_type(lines, datatype):
     return (f(x) for x in lines)
 
 
+def stdin_has_data():
+    return select.select([sys.stdin,],[],[],0.0)[0]
+
+
+def DataError(Exception):
+    pass
+
+
 def main(args):
-    with read_input(args['FILE'], args['-i']) as f:
-        data = cast_to_type(f, args['--datatype'])
-        plt = plot_hist(data, args['--title'], args['--xlabel'], int(args['--bins']))
-        if args['show']:
-            plt.show()
-        elif args['save']:
-            target = '{0}.png'.format(os.path.splitext(args['FILE'])[0])
-            plt.savefig(target)
-            print('Saved to {target}'.format(target=target))
-        return 0
+    if args.datafile:
+        with open(os.path.abspath(args.datafile)) as f:
+            lines = [x.strip() for x in f]
+    elif stdin_has_data():
+        lines = [x.strip() for x in sys.stdin]
+    else:
+        raise DataError('No data/data source specified. Either datafile or stdin is required')
+
+    data = cast_to_type(lines, args.type)
+    plt = plot_hist(data, args.title, args.xlabel, int(args.bins))
+    if args.action == 'show':
+        plt.show()
+    elif args.action == 'save':
+        target = '{0}.png'.format(args.title)
+        plt.savefig(target)
+        print('Saved to {target}'.format(target=target))
+    return 0
 
 
 if __name__ == '__main__':
-    args = docopt(__doc__, version=1.0)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("title", help="The title of the histogram")
+    parser.add_argument("xlabel", help="The label on the X-axis")
+    parser.add_argument("-d", "--datafile", help="Path to the input datafile")
+    parser.add_argument("-a", "--action", choices=['show', 'save'], help="The action to carry out", default="show")
+    parser.add_argument("-t", "--type", choices=['int', 'float'], default='int', help="Type of input on each line expected")
+    parser.add_argument("-b", "--bins", type=int, default=10, help="No. of bins")
+    args = parser.parse_args()
     sys.exit(main(args))
